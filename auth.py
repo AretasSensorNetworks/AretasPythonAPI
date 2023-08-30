5064718516c1aa77ed1c87d3909d46c8303c676a
ElDuderino <danpothier@gmail.com>
2023-08-29 22:49:31 -0700

    Update auth.py
    Fix for test_token. If this method is called before a token is acquired, it would trigger an error concatenating self.API_TOKEN as str (it is initialized to None)

--- auth.py
+++ auth.py
@@ -16,6 +16,8 @@ class APIAuth:
 
     def test_token(self):
         """test if the token is valid"""
+        if self.API_TOKEN is None:
+            return False
         api_response = requests.get(self.api_config.get_api_url() + "greetings/isloggedin",
                                     headers={"Authorization": "Bearer " + self.API_TOKEN})
 
