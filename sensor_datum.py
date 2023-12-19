95b1d41f29b14c633952aadff5b2e391b2b8dc7b
ElDuderino <danpothier@gmail.com>
2023-12-18 21:57:54 -0800

    Update sensor_datum.py
    Fixing formatting

--- sensor_datum.py
+++ sensor_datum.py
@@ -2,6 +2,7 @@ class SensorDatum:
     """
     The contract for a sensor data point from the API
     """
+
     def __init__(self, mac: int = None, data_type: int = None, timestamp: int = None, data: float = None):
         self.__mac = mac
         self.__data_type = data_type
@@ -16,26 +17,26 @@ class SensorDatum:
             self.get_data()
         )
 
-    def set_mac(self, mac:int):
+    def set_mac(self, mac: int):
         self.__mac = mac
 
-    def get_mac(self)->int:
+    def get_mac(self) -> int:
         return self.__mac
 
-    def set_type(self, data_type:int):
+    def set_type(self, data_type: int):
         self.__data_type = data_type
 
-    def get_type(self)->int:
+    def get_type(self) -> int:
         return self.__data_type
 
-    def set_timestamp(self, timestamp:int):
+    def set_timestamp(self, timestamp: int):
         self.__timestamp = timestamp
 
-    def get_timestamp(self)->int:
+    def get_timestamp(self) -> int:
         return self.__timestamp
 
-    def set_data(self, data:float):
+    def set_data(self, data: float):
         self.__data = data
 
-    def get_data(self)->float:
+    def get_data(self) -> float:
         return self.__data
